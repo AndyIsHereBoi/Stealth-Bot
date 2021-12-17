9929aaec9f5b093dbb0f485f033cfa91296e9858
Ender2K89 <ender2k89@gmail.com>
2021-12-17 20:25:53 +0100

    testing

--- cogs/economy.py
+++ cogs/economy.py
@@ -24,15 +24,18 @@ class Economy(commands.Cog):
         """Add money to a user's wallet"""
         balance = await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", user)
         await self.client.db.execute("UPDATE economy SET balance = $1 WHERE user_id = $2", balance + amount, user)
+        return await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", user)
 
     async def set_money(self, user: int, amount: int):
         """Set a user's wallet to a specific amount"""
         await self.client.db.execute("UPDATE economy SET balance = $1 WHERE user_id = $2", amount, user)
+        return await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", user)
 
     async def remove_money(self, user: int, amount: int):
         """Remove money from a user's wallet"""
         balance = await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", user)
         await self.client.db.execute("UPDATE economy SET balance = $1 WHERE user_id = $2", balance - amount, user)
+        return await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", user)
 
     @commands.command(
         help="Creates a balance for you if you don't have one.",
@@ -64,3 +67,32 @@ class Economy(commands.Cog):
 
         balance = await self.client.db.fetchval("SELECT balance FROM economy WHERE user_id = $1", member.id)
         await ctx.send(f"{'You have' if member.id == ctx.author.id else f'{member.mention} has'} {balance:,} money.")
+
+    @commands.group(
+        invoke_without_command=True,
+        help="Economy commands for the owner.",
+        aliases=['admineco', 'ecoadmin', 'eco_admin'])
+    async def admin_eco(self, ctx: CustomContext, member: typing.Union[discord.Member, discord.User] = None) -> discord.Message:
+        if ctx.invoked_subcommand is None:
+            await ctx.send_help(ctx.command)
+
+    @admin_eco.command(
+        name="add",
+        help="Adds money to the specified member's balance",
+        aliases=['give', '+'])
+    async def admin_eco_add(self, ctx, member: typing.Union[discord.Member, discord.User], amount: int) -> discord.Message:
+        await ctx.trigger_typing()
+
+        if member is None:
+            if ctx.message.reference:
+                member = ctx.message.reference.resolved.author
+            else:
+                member = ctx.author
+
+        if member.bot:
+            return await ctx.send("Bots don't have a balance.")
+
+        if not await self.client.db.fetchval("SELECT user_id FROM economy WHERE user_id = $1", member.id):
+            return await ctx.send(f"{'You dont' if member.id == ctx.author.id else f'{member.mention} doesnt'} have a balance.")
+
+        return await ctx.send(f"Successfully added {amount:,} money to {'your' if member.id == ctx.author.id else f'{member.mention}s'} balance. They now have {await self.add_money(member.id, amount):,} money.")
