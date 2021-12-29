31e861067e2b24c1c1c3313d214c0dddd66af48b
Ender2K89 <ender2k89@gmail.com>
2021-12-29 21:12:49 +0100

    adding image commands

--- cogs/images.py
+++ cogs/images.py
@@ -63,6 +63,96 @@ class Images(commands.Cog):
         return await ctx.send(embed=embed)
 
     @commands.command()
+    async def fox(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/fox")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def koala(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/koala")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def panda(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/panda----------")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def redpanda(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/redpanda")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def bird(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/birb")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def raccoon(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/raccoon")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def kangaroo(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/kangaroo")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def whale(self, ctx):
+        request = await self.client.session.get(f"https://some-random-api.ml/animal/cat")
+        data = await request.json()
+
+        embed = discord.Embed(description=str(data['fact']).replace(". ", ".\n"))
+        embed.set_image(url=data['image'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
+    async def duck(self, ctx):
+        request = await self.client.session.get(f"https://random-d.uk/api/v2/random")
+        data = await request.json()
+
+        embed = discord.Embed()
+        embed.set_image(url=data['url'])
+
+        return await ctx.send(embed=embed)
+
+    @commands.command()
     async def bunny(self, ctx):
         return
 
@@ -72,30 +162,6 @@ class Images(commands.Cog):
 
     @commands.command()
     async def lizard(self, ctx):
-        return
-
-    @commands.command()
-    async def fox(self, ctx):
-        return
-
-    @commands.command()
-    async def koala(self, ctx):
-        return
-
-    @commands.command()
-    async def panda(self, ctx):
-        return
-
-    @commands.command()
-    async def redpanda(self, ctx):
-        return
-
-    @commands.command()
-    async def duck(self, ctx):
-        return
-
-    @commands.command()
-    async def bird(self, ctx):
         return
 
     @commands.command(
