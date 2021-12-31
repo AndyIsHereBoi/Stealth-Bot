5f9eaf95a0dbbedc40a74aa27f345f0f7fe7c3a2
Ender2K89 <ender2k89@gmail.com>
2021-12-31 12:07:06 +0100

    some weird shit

--- cogs/events.py
+++ cogs/events.py
@@ -171,12 +171,6 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction: discord.Reaction, user: typing.Union[discord.Member, discord.User]):
-        if not user.guild:
-            return
-
-        if user.guild.id != 925067864241754132:
-            return
-
         current_colours = filter(lambda r: r in self.colours.values(), user.roles)
         await user.remove_roles(*current_colours, reason="Color role reaction.")
         await user.add_roles(self.colours[self.str(reaction.emoji)], reason="Color role reaction.")
