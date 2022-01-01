5611ad0edb470cf7c7955f7007a30b6243699237
Ender2K89 <ender2k89@gmail.com>
2022-01-01 23:09:16 +0100

    level system

--- cogs/levels.py
+++ cogs/levels.py
@@ -95,7 +95,7 @@ class Levels(commands.Cog):
             'level': user['level'],  # User current level
             'current_xp': 0,  # Current level minimum xp
             'user_xp': user['xp'],  # User current xp
-            'next_xp': round((4 * (user['level'] ** 3)) / 5),  # xp required for next level
+            'next_xp': 300 * user['level'],  # xp required for next level
             'user_position': 0,  # User position in leaderboard
             'user_name': str(member),  # username with descriminator
             'user_status': member.status.name,  # User status eg. online, offline, idle, streaming, dnd
