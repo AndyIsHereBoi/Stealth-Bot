29ed38f05ff00fc6286e77887c33daac5ec47aeb
Ender2K89 <ender2k89@gmail.com>
2022-01-10 16:35:02 +0100

    yes

--- cogs/events.py
+++ cogs/events.py
@@ -788,7 +788,15 @@ Command {ctx.command} raised the following error:
         if message.author.id != 555818548291829792:
             return
 
-        if re.match(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", message.content.lower):
+        regex = re.compile(
+            r'^(?:http|ftp)s?://'  # http:// or https://
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+            r'localhost|'  # localhost...
+            r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
+            r'(?::\d+)?'  # optional port
+            r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+        if re.match(regex, message.content.lower()) is not None:
             try:
                 return await message.delete()
 
